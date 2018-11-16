84af08de6a6b7fc413f667eea5a877a4f3550e25
Pluribus <Adam.kendall.wright@gmail.com>
2018-11-15 22:15:49 -0700

    pause menu added.  Currently "resume" "restart" and "quit" work

--- Assets/Menus.cs
+++ Assets/Menus.cs
@@ -1,16 +1,63 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Menus : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+
+    public GameObject pauseMenu;
+    bool pause = false;
+
+
+    // Use this for initialization
+    void Start ()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        if (Input.GetButtonDown("Escape"))
+        {
+            Pause();
+        }
+    }
+
+
+
+    public void Pause()  //called by escape key and "resume" button
+    {
+        pause = !pause;
+
+
+        if (pause)
+        {
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else if (!pause)
+        {
+            pauseMenu.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+
+
+
 }
