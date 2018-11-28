aa1e8ec0332a7e81733b35aaef43de2f4abf939e
Xillith <sixjedi@gmail.com>
2018-11-28 06:33:06 -0700

    Added a scene for a shop. Shop not yet fully implement but probably almost there.

---
+++ Assets/Inventory Scripts/InventoryItemDisplay.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryItemDisplay : MonoBehaviour {
+
+    public int ItemId;
+    public ItemClass Item;
+    public Text textName;
+    public Text ItemPrice;
+    //public Text ItemDescription;
+    //public Image ItemSprite;
+    
+
+	// Use this for initialization
+	void Start () {
+
+        //Item = ItemClass.GetItem(ItemSlot.WEAPON, 3);
+        //Debug.Log("Item Received " + Item.GetItemDesc());
+        //    Prime(Item);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void Prime(ItemClass item) {
+            this.Item = item;
+        
+            textName.text = item.GetItemName();
+        //if (ItemSprite != null)
+        //    ItemSprite.sprite = item.sprite;
+  
+            ItemPrice.text = "$"+item.GetGoldCost();
+        //ItemDescription.text = item.GetItemDesc();
+    }
+}
