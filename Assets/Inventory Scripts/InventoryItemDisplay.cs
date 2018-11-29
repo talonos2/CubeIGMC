63ed7b471cd9e10464ab628fc64cc250cac06d71
Xillith <sixjedi@gmail.com>
2018-11-28 17:09:52 -0700

    More work on equipment purchase screen. You can now kinda buy items (right now you can't select the item you want to buy though...) and have the charactersheet saved to disk. (Which character sheet is saved is defult right now. Still no data passing between scene's)

--- Assets/Inventory Scripts/InventoryItemDisplay.cs
+++ Assets/Inventory Scripts/InventoryItemDisplay.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,21 +10,68 @@ public class InventoryItemDisplay : MonoBehaviour {
     public ItemClass Item;
     public Text textName;
     public Text ItemPrice;
+    public bool Weapon;
+    public bool Shields;
+    public bool Engines;
+    public bool Armor;
+    public bool Power;
+    public PlayerCharacterSheet CurrentPlayer=new PlayerCharacterSheet();
+    public Transform PlayerStorage;
+    
     //public Text ItemDescription;
     //public Image ItemSprite;
-    
 
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start () {
+
+        RefreshEquipment();
+       
+
 
         //Item = ItemClass.GetItem(ItemSlot.WEAPON, 3);
         //Debug.Log("Item Received " + Item.GetItemDesc());
         //    Prime(Item);
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    }
+
+    public void RefreshEquipment()
+    {
+        if (PlayerStorage != null)
+        {
+            CurrentPlayer = PlayerStorage.GetComponent<PlayerBuyingEquipment>().GetPlayer();
+        }
+        //CurrentPlayer = PlayerBuyingEquipment.GetPlayer();
+        if (Weapon)
+        {
+            Item = ItemClass.GetItem(ItemSlot.WEAPON, CurrentPlayer.WeaponEquippedID);
+            Prime(Item);
+        }
+        if (Shields)
+        {
+            Item = ItemClass.GetItem(ItemSlot.SHIELDS, CurrentPlayer.ShieldEquippedID);
+            Prime(Item);
+        }
+        if (Engines)
+        {
+            Item = ItemClass.GetItem(ItemSlot.ENGINES, CurrentPlayer.EngineEquippedID);
+            Prime(Item);
+        }
+        if (Armor)
+        {
+            Item = ItemClass.GetItem(ItemSlot.ARMOR, CurrentPlayer.ArmorEquippedID);
+            Prime(Item);
+
+        }
+        if (Power)
+        {
+            Item = ItemClass.GetItem(ItemSlot.MISC, CurrentPlayer.MiscEquippedID);
+            Prime(Item);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
     public void Prime(ItemClass item) {
